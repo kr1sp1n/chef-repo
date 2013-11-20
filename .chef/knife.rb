9249dada69538098534eca1f31b3e2084fa6a6cc
kr1sp1n <krispinone@googlemail.com>
2013-11-20 16:30:09 +0100

    remove .chef folder

--- .chef/knife.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'solo'
-local_mode               true
-client_key               File.expand_path('../solo.pem', __FILE__)
-cache_type               'BasicFile'
-cache_options( :path => File.expand_path('../checksums', __FILE__))
-cookbook_path [ File.expand_path('../../cookbooks', __FILE__) ]
