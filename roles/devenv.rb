78718d767ed7075be8ef2607e4e90a10e67edf8b
kr1sp1n <krispinone@googlemail.com>
2013-11-20 13:32:57 +0100

    add base role

---
+++ roles/devenv.rb
@@ -0,0 +1,10 @@
+name "devenv"
+description "devenv role"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+run_list(
+  "role[base]"
+)
+# Attributes applied if the node doesn't have it set already.
+#default_attributes()
+# Attributes applied no matter what the node has set already.
+#override_attributes()
