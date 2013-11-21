d2382d35bcecf3f6c42a393c99f677dc85e00ecc
kr1sp1n <krispinone@googlemail.com>
2013-11-21 08:02:41 +0100

    add tmux to base role

--- roles/base.rb
+++ roles/base.rb
@@ -3,7 +3,9 @@ description "base role"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 run_list(
   "recipe[git]",
-  "recipe[zsh]"
+  "recipe[zsh]",
+  "recipe[ntp]",
+  "recipe[tmux]"
 )
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
